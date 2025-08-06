8dec2d7a58b157710477731e57a8c4a45fc246d7
Arman <armanndj9082360@gmail.com>
2025-08-06 20:53:33 +0530

    #datatypes

---
+++ Primitives/Primitives.java
@@ -0,0 +1,12 @@
+package Primitives;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno=64; 
+        char letter='r';
+        float marks=98.67f;
+        double LargeNumbers=4539030.28303;
+        long largeInteger=343908480849372L;
+        boolean check=false;
+    }
+}
